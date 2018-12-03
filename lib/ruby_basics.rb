ecafc2d28a9c74dd20533caeb0a741a58d02b876
Anthony Gregg <agregg22@gmail.com>
2018-12-02 20:16:19 -0500

    Complete

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,38 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+  value = (num1 / num2)
+  value
 end
 
 def assign_variable(value)
-  
+name = value
+name
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
+  phrase
 end
 
-def greeting
-
+def greeting(greeting,name)
+  greeting = "Hi there, "
+  name = "Anthony!"
+  greeting << name
+  puts greeting
 end
 
 def return_a_value
-
+value = "Nice"
+value
 end
 
 def last_evaluated_value
-
+  value = "Nice"
+  last_value = "expert"
+  last_value
 end
 
-def pizza_party
-
+def pizza_party(pizza = "cheese")
+  pizza
 end
+
+pizza_party("pepperoni")
